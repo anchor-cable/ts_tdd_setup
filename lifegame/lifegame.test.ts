c051bfdcc8cfa97461f10b7de2f2058217356764
Tomoya Suzuki <forever.blue.moon@gmail.com>
2020-10-26 20:49:33 +0900

    tddモブプロ会進捗
    Co-authored-by: d2verb <licprog73@gmail.com>

--- lifegame/lifegame.test.ts
+++ lifegame/lifegame.test.ts
@@ -1,5 +1,24 @@
-import { Greeter } from './lifegame';
+import { toLifeGameList, countSurroundingALive } from "./lifegame";
 
-test('test', () => {
-    expect(new Greeter('Taro').getMessage()).toBe('Hello Taro');
+test("ライフゲームフォーマットに沿った標準入力を受け取って配列化する", () => {
+  expect(toLifeGameList("*-*\n***\n---")).toStrictEqual([
+    ["*", "-", "*"],
+
+    ["*", "*", "*"],
+
+    ["-", "-", "-"],
+  ]);
+});
+
+test("セルを渡すと周りに生きているセルが幾つあるのか数え上げる", () => {
+  const x = 0; // 原点(左上)
+  const y = 0; // 原点(左上)
+  const world = [
+    ["*", "-", "*"],
+
+    ["*", "*", "*"],
+
+    ["-", "-", "-"],
+  ];
+  expect(countSurroundingALive(x, y, world)).toBe(2);
 });
--- lifegame/lifegame.ts
+++ lifegame/lifegame.ts
@@ -1,7 +1,39 @@
-export class Greeter {
-    constructor(private name: string) {}
+export function toLifeGameList(input: string): string[][] {
+  return input.split("\n").map((line) => line.split(""));
+}
 
-    getMessage() {
-        return `Hello ${this.name}`;
+export function countSurroundingALive(
+  x: number,
+  y: number,
+  world: string[][]
+): number {
+  let count = 0;
+
+  // x, yからみて、'*'どうかを調べるマスの相対的位置
+  const move = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+  ];
+
+  move.forEach(([dx, dy]) => {
+    // nx, ny ... 見ているマスの座標
+    const nx = x + dx;
+    const ny = y + dy;
+
+    // worldからはみ出していない
+    if (0 <= nx && nx <= 2 && 0 <= ny && ny <= 2) {
+      // かつ、'*'である
+      if (world[ny][nx] === "*") {
+        count++;
+      }
     }
+  });
+
+  return count;
 }
